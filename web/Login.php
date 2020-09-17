a719c1cc14622880fc883a7a1f1fcf12d2623c5f
tgh1107 <8066370+tgh1107@users.noreply.github.com>
2020-09-17 19:58:21 +1000

    add more files

--- web/Login.php
+++ web/Login.php
@@ -1,7 +1,98 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is already logged in, if yes then redirect him to welcome page
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+  header("location: PendingOrder.php");
+  exit;
+}
+ 
+// Include config file
+require_once "./service/config.php";
+ 
+// Define variables and initialize with empty values
+$username = $password = "";
+$username_err = $password_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Check if username is empty
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter username.";
+    } else{
+        $username = trim($_POST["username"]);
+    }
+    
+    // Check if password is empty
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter your password.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    // Validate credentials
+    if(empty($username_err) && empty($password_err)){
+        // Prepare a select statement
+        $sql = "SELECT id, username, password FROM users WHERE username = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            
+            // Set parameters
+            $param_username = $username;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Store result
+                mysqli_stmt_store_result($stmt);
+                
+                // Check if username exists, if yes then verify password
+                if(mysqli_stmt_num_rows($stmt) == 1){                    
+                    // Bind result variables
+                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+                    if(mysqli_stmt_fetch($stmt)){
+                        if(password_verify($password, $hashed_password)){
+                            // Password is correct, so start a new session
+                            session_start();
+                            
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["username"] = $username;                            
+                            
+                            // Redirect user to welcome page
+                            //header("location: welcome.php");
+							header("location: PendingOrder.php");
+                        } else{
+                            // Display an error message if password is not valid
+                            $password_err = "The password you entered was not valid.";
+                        }
+                    }
+                } else{
+                    // Display an error message if username doesn't exist
+                    $username_err = "No account found with that username.";
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+    
+    // Close connection
+    mysqli_close($link);
+}
+?>
+ 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <meta charset="UTF-8">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
@@ -12,9 +103,15 @@
 	<link rel="stylesheet" href="./css/font-awesome.css">
 
     <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-    
+    <title>Login</title>
+
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ padding: 20px; }
+    </style>
 </head>
 <body>
+
 	<!--NAVIGATION-->
 	<div class="header-main bg-white">
     	<div class="container">
@@ -49,63 +146,34 @@
     <div class="header-spacing-helper" style="height: 90px;">
 	</div>
 	
-	
-
-<?php
-session_start();
-?>
-
-<?php
-	require './service/database_connection.php';
-	if (isset($_POST["btn_submit"])) {
-		$username = $_POST["username"];
-		$password = $_POST["password"];
-                //protect from sql injection
-		$username = strip_tags($username);
-		$username = addslashes($username);
-		$password = strip_tags($password);
-		$password = addslashes($password);
-		if ($username == "" || $password =="") {
-			echo "username or password can not be blank";
-		}else{
-			$sql = "select * from users where username = '$username' and password = '$password' ";
-			$query = mysqli_query($conn,$sql);
-			$num_rows = mysqli_num_rows($query);
-			if ($num_rows==0) {
-				echo "Wrong username or password";
-			}else{
-				$_SESSION['username'] = $username;
-                header('Location: PendingOrder.php');
-			}
-		}
-	}
-?>
-	<!--FORM-->
+		<!--FORM-->
 	<div class="login">
     <div class="form"> 
+		<div class="wrapper">
 		<h2>ARE YOU A MEMBER?</h2> 
 		<h3> Log In here</h3>
-		<form method="POST" action="Login.php">
-			<table border ="0">
-				<tr>
-					<td align="left"> Username: </td>
-					<td><input type="text" name="username" class="info" id=""></td>
-				</tr>
-				<tr>
-					<td align="left"> Password:</td>
-					<td><input type="password" name="password" class="info" id="" ></td>
-				</tr>
-				<tr>
-	    			<td colspan="2" align="center"> <input type="submit" name="btn_submit" value="login"></td>
-	    		</tr>
-			</table>
-		</form>
-		<!--<a class="click" href=""> LOG IN</a>-->
-		<p> Not a member yet? <a href="Register.php">Register now</a></p>
+
+        <p>Please fill in your credentials to login.</p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                <label>Username</label>
+                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+                <span class="help-block"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control">
+                <span class="help-block"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Login">
+            </div>
+            <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
+        </form>
+		</div> 
 	</div>
 
 	</div>
-	
 
 	<!--FOOTER-->
 	<div class="row1">
@@ -119,5 +187,6 @@ session_start();
 			<p>TELEPHONE Sri Lanka- +94 11 252 1394 </br> TELEPHONE Australia - +61 41 725 4352 </br> EMAIL- info@tritonglobalshipping.com.au </br> WEB-www.tritonglobalshipping.com.au</p>
 		</div>
 	</div>
+	
 </body>
 </html>
---
+++ web/Login___1.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+	<title>LOGIN</title>
+	<link rel="stylesheet" href="./css/vendor.css">
+	<link rel="stylesheet" href="./css/main.css">
+	<link rel="stylesheet" href="./css/style.css">
+	<link rel="stylesheet" href="./css/font-awesome.css">
+
+    <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+    
+</head>
+<body>
+	<!--NAVIGATION-->
+	<div class="header-main bg-white">
+    	<div class="container">
+    		<div class="row">
+    			<nav class="navbar navbar-expand-lg navbar-light w-100" id="header-navbar">
+					<img src="./image/logo.png">
+    				<a class="navbar-brand font-weight-bold">TRITON GLOBAL </br> &nbsp; &nbsp; SHIPPING</a> 
+    				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+    					<ul class="navbar-nav ml-auto">
+    						<li class="nav-item">
+    							<a class="nav-link text-uppercase" data-toggle="none" href="index.php">HOME</a></li>
+    						<li class="nav-item">
+    							<a class="nav-link text-uppercase" data-toggle="none" href="AboutUs.php">ABOUT US</a>
+    						</li>
+    						<li class="nav-item">
+    							<a class="nav-link text-uppercase" data-toggle="none" href="ContactUs.php">CONTACT US</a>
+    						</li>
+    						<li class="nav-item">
+    							<a class="nav-link text-uppercase" data-toggle="none" href="Ournetworks.php">OUR NETWORKS</a>
+    						</li>
+    						<li class="nav-item"><a class="nav-link text-uppercase" data-toggle="none" href="Order.php">ORDER</a>
+    						</li>
+    						<li class="nav-item"><a class="nav-link text-uppercase" data-toggle="none" href="Login.php">LOGIN</a>
+    						</li>
+    					</ul>
+    				</div>
+    			</nav>
+    		</div>
+    	</div>
+    </div>
+    </section>
+    <div class="header-spacing-helper" style="height: 90px;">
+	</div>
+	
+	
+
+<?php
+session_start();
+?>
+
+<?php
+	require_once './service/database_connection.php';
+	
+	if (isset($_POST["btn_submit"])) {
+		$username = $_POST["username"];
+		$password = $_POST["password"];
+                //protect from sql injection
+		$username = strip_tags($username);
+		$username = addslashes($username);
+		$password = strip_tags($password);
+		$password = addslashes($password);
+		if ($username == "" || $password =="") {
+			echo "username or password can not be blank";
+		}else{
+			$sql = "select * from users where username = '$username' and password = '$password' ";
+			$query = mysqli_query($conn,$sql);
+			$num_rows = mysqli_num_rows($query);
+			if ($num_rows==0) {
+				echo "Wrong username or password";
+			}else{
+				$_SESSION['username'] = $username;
+                header('Location: PendingOrder.php');
+			}
+		}
+	}
+?>
+	<!--FORM-->
+	<div class="login">
+    <div class="form"> 
+		<h2>ARE YOU A MEMBER?</h2> 
+		<h3> Log In here</h3>
+		<form method="POST" action="Login.php">
+			<table border ="0">
+				<tr>
+					<td align="left"> Username: </td>
+					<td><input type="text" name="username" class="info" id=""></td>
+				</tr>
+				<tr>
+					<td align="left"> Password:</td>
+					<td><input type="password" name="password" class="info" id="" ></td>
+				</tr>
+				<tr>
+	    			<td colspan="2" align="center"> <input type="submit" name="btn_submit" value="login"></td>
+	    		</tr>
+			</table>
+		</form>
+		<!--<a class="click" href=""> LOG IN</a>-->
+		<p> Not a member yet? <a href="Register.php">Register now</a></p>
+	</div>
+
+	</div>
+	
+
+	<!--FOOTER-->
+	<div class="row1">
+		<div class="columnpic">
+			<img src="./image/logo1.png">	
+		</div>
+		<div class="column">
+			<p>TGS 2017 Copyright (c) </br> TRITON GLOBAL SHIPPING (PVT) LTD </br> Office: Level 36, The Riparian Plaza 71 Eagle Street BRISBANE, QLD. 4000 AUSTRALIA</br> </p>
+		</div>
+		<div class="column">
+			<p>TELEPHONE Sri Lanka- +94 11 252 1394 </br> TELEPHONE Australia - +61 41 725 4352 </br> EMAIL- info@tritonglobalshipping.com.au </br> WEB-www.tritonglobalshipping.com.au</p>
+		</div>
+	</div>
+</body>
+</html>
