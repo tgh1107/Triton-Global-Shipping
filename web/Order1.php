2d447277b0cff6d387e852686024cfeb0e8993a9
thanhmai3896 <68112174+thanhmai3896@users.noreply.github.com>
2020-09-23 12:09:27 +1000

    Add files via upload

---
+++ web/Order1.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+	<title>REQUEST AN ORDER </title>
+	<link rel="stylesheet" href="./vendor.css">
+	<link rel="stylesheet" href="./main.css">
+	<link rel="stylesheet" href="./style.css">
+	<link rel="stylesheet" href="./font-awesome.css">
+
+    
+</head>
+<body>
+	<!--NAVIGATION-->
+	<div class="header-main bg-white">
+    	<div class="container">
+    		<div class="row">
+    			<nav class="navbar navbar-expand-lg navbar-light w-100" id="header-navbar">
+					<img src="logo.png">
+    				<a class="navbar-brand font-weight-bold">TRITON GLOBAL </br> &nbsp; &nbsp; SHIPPING</a> 
+    				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+    					<ul class="navbar-nav ml-auto">
+    						<li class="nav-item">
+    							<a class="nav-link text-uppercase" data-toggle="none" href="index.html">HOME</a></li>
+    						<li class="nav-item">
+    							<a class="nav-link text-uppercase" data-toggle="none" href="">ABOUT US</a>
+    						</li>
+    						<li class="nav-item">
+    							<a class="nav-link text-uppercase" data-toggle="none" href="ContactUs.html">CONTACT US</a>
+    						</li>
+    						<li class="nav-item">
+    							<a class="nav-link text-uppercase" data-toggle="none" href="Ournetworks.html">OUR NETWORKS</a>
+    						</li>
+    						<li class="nav-item"><a class="nav-link text-uppercase" data-toggle="none" href="">ORDER</a>
+    						</li>
+    						<li class="nav-item"><a class="nav-link text-uppercase" data-toggle="none" href="Login.html">LOGIN</a>
+    						</li>
+    					</ul>
+    				</div>
+    			</nav>
+    		</div>
+    	</div>
+    </div>
+    </section>
+    <div class="header-spacing-helper" style="height: 90px;">
+	</div>
+	
+	<!--BODY-->
+	
+	<div class="network">
+		<h1> REQUEST AN ORDER</h1>
+	</div>
+	<h2 style="margin-top: 30px; text-align: center;">READY TO SHIP YOUR PACKAGE? </h2>
+	<div class="order_form">
+		<p> Fields marked * are required</p>
+		<form>
+			<div>
+			<div class="cus_info">
+				<h4> SENDER DETAILS</h4>
+				<label> Name*</label><br>
+				<input type="text" name=""><br>
+				<label> Email*</label><br>
+				<input type="text" name=""><br>
+				<label> Contact number*</label><br>
+				<input type="text" name=""><br>
+				<label> Address*</label><br>
+				<input type="text" name="">
+			</div>
+			
+			<div class="cus_info">
+				<h4> RECEIVER DETAILS</h4>
+				<label> Name*</label><br>
+				<input type="text" name=""><br>
+				<label> Email</label><br>
+				<input type="text" name=""><br>
+				<label> Contact number*</label><br>
+				<input type="text" name=""><br>
+				<label> Address*</label><br>
+				<input type="text" name="">
+			</div>
+		</div>
+			<div style="margin-left: 8px"><h4> PACKAGE DETAILS *</h4></div>
+			<div class="package_info">	
+				
+				<label> Package type</label> <br>
+				<label for="">Hazardous item ?</label>
+				<select name="" id="">
+  					<option value="">Hazardous</option>
+  					<option value="">Non-hazardous</option>	
+				</select> <br>
+				<label for="">Package type</label>
+				<select name="" id="">
+  					<option value="">Solid</option>
+					<option value="">Liquid</option>	
+					<option value="">Gas</option>
+				</select> <br>
+				<label for="">Food ?</label>
+				<select name="" id="">
+  					<option value="">Yes</option>
+					<option value="">No</option>	
+				</select> <br>
+				<label class="form_title"> Weight(kg)</label><br>
+				<input type="text" name=""><br>
+				<label class="form_title"> Length(cm)</label><br>
+				<input type="text" name="">
+			
+			</div>
+			<div class="package_info">
+				<label> Width(cm)</label> <br>
+				<input type="text" name=""> <br>
+				<label class="form_title"> Height(cm)</label> <br>
+				<input type="text" name=""><br>
+				<label class="form_title"> Quantity</label> <br>
+				<input type="text" name="">
+			
+			</div>
+			<div class="package_info"> 
+				<label> Day of dispatch</label> <br>
+				<input type="date" name=""> <br>
+				<label class="form_title"> Day of arrival</label> <br>
+				<input type="date" name=""> <br>
+				<label class="form_title"> Level of urgency</label> <br>
+				<select>
+					<option value="level1"> In 1 week</option>
+					<option value="level2"> In 2 weeks</option>
+					<option value="level3"> In 1 month</option>
+				</select>
+			
+				
+			
+			</div>
+			<div style="margin:auto"> 
+			
+				<button class="order_button"> SUBMIT ORDER</button>
+			</div>
+		</form>
+
+	</div>
+	
+
+
+	
+
+	<!--FOOTER-->
+	<div class="row1">
+		<div class="columnpic">
+			<img src="logo1.png">	
+		</div>
+		<div class="column">
+			<p>TGS 2017 Copyright (c) </br> TRITON GLOBAL SHIPPING (PVT) LTD </br> Office: Level 36, The Riparian Plaza 71 Eagle Street BRISBANE, QLD. 4000 AUSTRALIA</br> </p>
+		</div>
+		<div class="column">
+			<p>TELEPHONE Sri Lanka- +94 11 252 1394 </br> TELEPHONE Australia - +61 41 725 4352 </br> EMAIL- info@tritonglobalshipping.com.au </br> WEB-www.tritonglobalshipping.com.au</p>
+		</div>
+	</div>
+</body>
+</html>
