8cf7d16ae295716e549d540732c45cfb9e9f6240
thanhmai3896 <68112174+thanhmai3896@users.noreply.github.com>
2020-12-06 23:54:03 +1000

    Update Ournetworks.php

--- web/Ournetworks.php
+++ web/Ournetworks.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
 	<title>TRITON GLOBAL SHIPPING</title>
 	<link rel="stylesheet" href="./vendor.css">
-	<link rel="stylesheet" href="./main.css">
+	<link rel="stylesheet" href="./css/main.css">
 	<link rel="stylesheet" href="./style.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> 
 	<link href="https://use.fontawesome.com/releases/v5.0.4/css/all.css" rel="stylesheet"> 
