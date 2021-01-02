aa54e19bf8561dffdd1bad6dca1e9e7c5b1887ef
thanhmai3896 <68112174+thanhmai3896@users.noreply.github.com>
2021-01-02 23:14:09 +1000

    Update index.php

--- web/index.php
+++ web/index.php
@@ -220,4 +220,18 @@
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
-    gtag('config', 'UA-176058743-1');</script><script src="./vendor.js.download"></script><script src="./polyfills.js.download"></script><script src="./app.js.download"></script></body></html>
+    gtag('config', 'UA-176058743-1');</script><script src="./vendor.js.download"></script><script src="./polyfills.js.download"></script><script src="./app.js.download"></script>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5fec1448df060f156a9200fb/1eqp2ufgh';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+</body></html>
